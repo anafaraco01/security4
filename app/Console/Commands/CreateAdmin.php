7469edb105431af7f605eb43e845c7ea261462c8
anafaraco01 <rive0008@hz.nl>
2023-07-04 21:59:59 +0200

    Last Commit

--- app/Console/Commands/CreateAdmin.php
+++ app/Console/Commands/CreateAdmin.php
@@ -14,50 +14,51 @@ class CreateAdmin extends Command
      *
      * @var string
      */
-    protected $signature = 'make:admin {UserEmail=admin@hz.nl} {UserPassword=123qweASD###}';
+    protected $signature = 'make:admin {AdminEmail=admin@hz.nl} {AdminPassword=123qweASD###}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Created the first admin account with the given email and password.';
+    protected $description = 'Creates admin accounts with the given email and password.';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $userEmail = $this->argument('UserEmail');
-        $userPassword = $this->argument('UserPassword');
+        $email = $this->argument('AdminEmail');
+        $password = $this->argument('AdminPassword');
 
-        if (preg_match("#^[a-zA-Z0-9]+$#", $userEmail)) {
-            $userEmail .= '@example.com';
+        if (preg_match("#^[a-zA-Z0-9]+$#", $email)) {
+            $email .= '@example.com';
         }
 
-        if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
-            $existingUser = User::where('email', $userEmail)->first();
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $existingUser = User::where('email', $email)->first();
             if (! $existingUser) {
-                $validator = Validator::make(['password' => $userPassword], [
+                $validator = Validator::make(['password' => $password], [
                     'password' => ['required', 'string', 'min:12', 'regex:/[a-z]/', 'regex:/[A-Z]/', 'regex:/[0-9]/', 'regex:/[@$!%*#?&]/'],
                 ]);
 
                 if ($validator->fails()) {
-                    echo "   \nERROR | Password validation failed. The password must be at least 12 characters and contain at least one uppercase character, one number, and one special character. | ERROR\n";
+                    echo "   \nERROR | Password validation failed. The password must be at least 12 characters and contain at least one uppercase character,
+                    one number, and one special character. | ERROR\n";
                     return;
                 }
 
                 User::create([
-                    "name" => strstr($userEmail,'@',true),
-                    "email" => $userEmail,
-                    "password" => Hash::make($userPassword),
+                    "name" => strstr($email,'@',true),
+                    "email" => $email,
+                    "password" => Hash::make($password),
                     "role" => "admin"
                 ]);
                 echo "\n{ Laravel | Security4 |\n";
                 echo "|_ Admin\n    \_Create\n";
-                echo "  \n| Admin account created: $userEmail | $userPassword\n";
-            } else echo "   \nERROR | An account already uses that email: $userEmail | ERROR\n";
-        } else echo "   \nERROR | The email you entered is not in valid format: { $userEmail } - Correct format: { email@webite.com } | ERROR\n";
+                echo "  \n| Admin account created: $email | $password\n";
+            } else echo "   \nERROR | An account already uses that email: $email | ERROR\n";
+        } else echo "   \nERROR | The email you entered is not in valid format: { $email } - Correct format: { email@webite.com } | ERROR\n";
 
         echo"\n}";
     }
